33f7aa8508ee236ee621dcb12f06dd7c5365ab13
Andrey <andronshch030701@mail.ru>
2022-02-19 04:16:21 +0300

    Add Pest class and some changes in other classes' structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+from typing import Union
 import time
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class Garden:
@@ -8,8 +9,20 @@ class Garden:
     __fertilizers: int
 
     def __init__(self, fertilizers: int = 10) -> None:
-        self.__gardeners_list = []
         self.__fertilizers = fertilizers
+
+    def use_fertilizers(self, plant: Union[TomatoBush, CucumberBush]) -> None:
+        plant.set_pest_status(False)
+        plant.update_growing_state()
+        self.__fertilizers -= 1
+
+
+class Pest:
+    __pest_location: Union[TomatoBush, CucumberBush]
+
+    def set_pest_location(self, location: Union[TomatoBush, CucumberBush]) -> None:
+        self.__pest_location = location
+        location.set_pest_status(True)
 
 
 class Weather:
@@ -25,47 +38,93 @@ class Weather:
     def set_drought_weather(self) -> None:
         self.__actual_weather = self.__weather_list[2]
 
+    def get_weather_status(self) -> str:
+        return self.__actual_weather
+
 
 class Plant(ABC):
     __growing_states: tuple = ('flower', 'fetal formation', 'fetus (grown)')
     __growing_states_counter: int
-    # __growing_time: int
+    __growing_time: int
     __growing_state: str
+    __is_pest: bool
+    __plant_type: str
+    __number_of_fetuses: int
+    __water_scale: int = 10
+    __fertilizer_scale: int = 5
 
-    def __init__(self):
+    def __init__(self, plant_type: str, growing_time: int, number_of_fetuses: int) -> None:
+        self.__growing_states_counter = 0
+        self.__plant_type = plant_type
+        self.__growing_time = growing_time
+        self.__number_of_fetuses = number_of_fetuses
+        self.__is_pest = False
+
+    def update_growing_state(self) -> None:
+        self.__growing_state = self.__growing_states[self.__growing_states_counter]
+
+    def set_pest_status(self, status: bool) -> None:
+        self.__is_pest = status
+
+    def grow(self) -> None:
+        self.__water_scale -= 1
+        start: float = time.time()
+        while True:
+            end: float = time.time()
+            while start - end != self.__growing_time:
+                pass
+            self.__growing_states_counter += 1
+            self.update_growing_state()
+            if self.__growing_state == 'fetus (grown)':
+                print(f'Your {self.__plant_type} are grown! You can pick them up!')
+                break
+            else:
+                self.grow()
+
+    def pick_up(self) -> None:
+        self.__fertilizer_scale -= 1
         self.__growing_states_counter = 0
         self.update_growing_state()
-
-    # @abstractmethod
-    # def get_growing_time(self, growing_time: int) -> int:
-    #     pass
-
-    def update_growing_state(self):
-        self.__growing_state = self.__growing_states[self.__growing_states_counter]
+        if self.__is_pest == True:
+            print(f'Your {self.__plant_type} was attacked by pests,'
+                  f' so you pick up only {self.__number_of_fetuses / 2} {self.__plant_type}!')
+        else:
+            print(f'You pick up {self.__number_of_fetuses} {self.__plant_type}!')
 
 
 class TomatoBush(Plant):
-    __number_of_tomatoes: int
-
-    def __init__(self, number_of_tomatoes: int = 6) -> None:
-        super().__init__()
-        self.__number_of_tomatoes = number_of_tomatoes
+    def __init__(self) -> None:
+        super().__init__('tomatoes', 30, 6)
         self.__growing_state = self.__growing_states[0]
 
-    def grow(self) -> None:
-        start: float = time.time()
-        end: float = time.time()
-        while start - end != 30:
-            pass
-        self.__growing_states_counter += 1
-        self.__growing_state = self.__growing_states[self.__growing_states_counter]
-        if self.__growing_state == 'fetus (grown)':
-            print('Your tomatoes are grown!')
 
-    def pick_up(self) -> None:
-        self.__growing_states_counter = 0
-        self.__growing_state = self.__growing_states[self.__growing_states_counter]
-        print('You pick up 6 tomatoes!')
+class CucumberBush(Plant):
+    def __init__(self) -> None:
+        super().__init__('cucumbers', 45, 8)
+        self.__growing_state = self.__growing_states[0]
+
+    # def grow(self) -> None:
+    #     start: float = time.time()
+    #     while True:
+    #         end: float = time.time()
+    #         while start - end != self.__growing_time:
+    #             pass
+    #         self.__growing_states_counter += 1
+    #         self.update_growing_state()
+    #         if self.__growing_state == 'fetus (grown)':
+    #             print(f'Your {self.__plant_type} are grown!')
+    #             break
+    #         else:
+    #             self.grow()
+
+    # def pick_up(self) -> None:
+    #     self.__growing_states_counter = 0
+    #     self.update_growing_state()
+    #     if self.__is_pest == True:
+    #         print(f'Your {self.__plant_type} was attacked by pests,'
+    #               f' so you pick up only {self.__number_of_fetuses / 2} {self.__plant_type}!')
+    #     else:
+    #         print(f'You pick up {self.__number_of_fetuses} {self.__plant_type}!')
         # storage.tomatoes += 6
 
     # def set_growing_time(self, growing_time: int) -> None:
